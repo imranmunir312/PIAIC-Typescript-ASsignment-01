5f58853855d399de8cfaeddbcc129663c71f5237
imranmunir312 <imranmunir312@gmail.com>
2023-07-11 16:17:30 +0500

    Question 20 completed

--- index.ts
+++ index.ts
@@ -105,3 +105,70 @@ guests.push("Sameed");
 guests.forEach((guest: String) =>
   console.log(`Hi ${guest}, I would like to invite you to my dinner party`)
 );
+
+// Question 17
+console.log("I can only invite two people to my dinner party");
+
+while (guests.length > 2) {
+  console.log(`Sorry ${guests.pop()}, I can't invite you to dinner`);
+}
+
+guests.forEach((guest: String) =>
+  console.log(
+    `Hi ${guest}, I would still like to invite you to my dinner party`
+  )
+);
+
+guests.pop();
+guests.pop();
+
+console.log(guests);
+
+// Question 18
+
+let places: string[] = [
+  "Time Square, New York",
+  "Central Park, New York",
+  "Toronto",
+  "Madina",
+  "Taj Mehal, Agra",
+];
+
+console.log("Original Order: ", places);
+
+console.log("Alphabetical Order:", places.slice().sort());
+
+console.log("Original Order: ", places);
+
+console.log(
+  "Reverse Alphabetical Order:",
+  places.slice().sort((a, b) => b.localeCompare(a))
+);
+
+console.log("Original Order: ", places);
+
+places = places.reverse();
+
+console.log("Reverse Order: ", places);
+
+places = places.reverse();
+
+console.log("Reverse Order Again list back to original: ", places);
+
+places = places.sort();
+
+console.log("Alphabetical Order Original List: ", places);
+
+places.sort((a, b) => b.localeCompare(a));
+
+console.log("Reverse Alphabetical Order Original List: ", places);
+
+// Question 19
+
+console.log(`${guests.length} guests are invited to my dinner party`);
+
+// Question 20
+
+const rivers: string[] = ["indus", "nile", "amazon", "yangtze", "jhelum"];
+
+// Question 21
